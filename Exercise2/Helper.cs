1d343ed0fd98bc9364808a6f46eed209ee2bcc11
NinaAlejandra <niinaa___@hotmail.com>
2023-09-18 16:58:40 +0200

    Adding methods for 3 out of 4 cases and a helper file

---
+++ Exercise2/Helper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Helper
+    {
+        public static int CalculatePrice(int age)
+        {
+            if (age < 20)
+            {
+                return 80;
+            }
+
+            else if (age > 64)
+            {
+                return 90;
+            }
+
+            else
+            {
+                return 120;
+            }
+        }
+
+        public static int TicketType(string goBack)
+        {
+            Console.WriteLine("Ange din ålder");
+            string input = Console.ReadLine();
+            int age;
+            if (int.TryParse(input, out age))
+            {
+                int price = Helper.CalculatePrice(age);
+                Console.WriteLine($"Pris: {price}kr");
+
+            }
+            else
+            {
+                Console.WriteLine("Ogiltig input. Ange heltal");
+            }
+
+            Console.WriteLine(goBack);
+            Console.ReadKey(true);
+            return age;
+        }
+
+        public static void RepeatRandomText()
+        {
+            Console.WriteLine("Ange en random text");
+            string randomTextInput = Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(randomTextInput);
+            }
+        }
+
+        public static void ExtractThirdWord()
+        {
+            Console.WriteLine("Skriv en mening på minst tre ord");
+            var userInput = Console.ReadLine(); //Takes in the user input in a var
+            string[] sentence = userInput.Split(' '); //This splits the word in to an array
+            var thirdWord = sentence[2]; //And This retrieves the third word at index 2, since index starts at 0
+
+            {
+                Console.Write($"\nTredje ordet i din mening:{thirdWord}");
+            }
+        }
+
+    }
+}
--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise2
+﻿
+namespace Exercise2
+   
 {
     internal class Program
     {
@@ -6,35 +8,38 @@
         {
             ConsoleKeyInfo key; //This variable holds information about the key pressed by the user
             int option = 0; //This variable keeps track of current selected option. 
-            bool isSelected = false; // This indicates whether the user has made a selection
+            string goBack = "\nTryck på valfri tangent för att återgå till huvudmenyn...";
+            bool isProgramRunning = true;
+            while (isProgramRunning) {
 
-            while (!isSelected) { //This is the main loop that displays the menu and processes user input until valid selection is made
+                bool isSelected = false; // This indicates whether the user has made a selection
+                while (!isSelected) { //This is the main loop that displays the menu and processes user input until valid selection is made
 
-                //Console UI, handles the consoles appearance and displays the menu options
+                    //Console UI, handles the consoles appearance and displays the menu options
                     Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Väkommen till Ninas program");
-                Console.ResetColor();
-                Console.WriteLine("\nDu kommer nu att få några val att välja bland");
-                Console.WriteLine("\nVälj:\n\u001b[34m0\u001b[0m för att avsluta, \n\u001b[34m1\u001b[0m för biljettpriser, \n\u001b[34m2\u001b[0m för kostnad per grupp, \n\u001b[34m3\u001b[0m för att upprepa en text 10 ggr och \n\u001b[34m4\u001b[0m för att få ut tredje ordet i en mening");
-                Console.WriteLine("\n\u001b[36mAnvänd upp och ner pilarna för att navigera och Enter för att välja\u001b[0m");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Väkommen till Ninas program");
+                    Console.ResetColor();
+                    Console.WriteLine("\nDu kommer nu att få några val att välja bland");
+                    Console.WriteLine("\nVälj:\n\u001b[34m0\u001b[0m för att avsluta, \n\u001b[34m1\u001b[0m för biljettpriser, \n\u001b[34m2\u001b[0m för kostnad per grupp, \n\u001b[34m3\u001b[0m för att upprepa en text 10 ggr och \n\u001b[34m4\u001b[0m för att få ut tredje ordet i en mening");
+                    Console.WriteLine("\n\u001b[36mAnvänd upp och ner pilarna för att navigera och Enter för att välja\u001b[0m");
 
-                string option0 = (option == 0 ? "\n\u001b[34mVal 0 - Avsluta programmet\u001b[0m" : "\nVal 0");
-                string option1 = (option == 1 ? "\u001b[34mVal 1 - Biljettpriser\u001b[0m" : "Val 1");
-                string option2 = (option == 2 ? "\u001b[34mVal 2 - Pris per antal besökare\u001b[0m" : "Val 2");
-                string option3 = (option == 3 ? "\u001b[34mVal 3 - Upprepa text 10 ggr\u001b[0m" : "Val 3");
-                string option4 = (option == 4 ? "\u001b[34mVal 4 - Få ut tredje ordet\u001b[0m" : "Val 4");
+                    string option0 = (option == 0 ? "\n\u001b[34mVal 0 - Avsluta programmet\u001b[0m" : "\nVal 0");
+                    string option1 = (option == 1 ? "\u001b[34mVal 1 - Biljettpriser\u001b[0m" : "Val 1");
+                    string option2 = (option == 2 ? "\u001b[34mVal 2 - Pris per antal besökare\u001b[0m" : "Val 2");
+                    string option3 = (option == 3 ? "\u001b[34mVal 3 - Upprepa text 10 ggr\u001b[0m" : "Val 3");
+                    string option4 = (option == 4 ? "\u001b[34mVal 4 - Få ut tredje ordet\u001b[0m" : "Val 4");
 
-                Console.WriteLine(option0);
-                Console.WriteLine(option1);
-                Console.WriteLine(option2);
-                Console.WriteLine(option3);
-                Console.WriteLine(option4);
+                    Console.WriteLine(option0);
+                    Console.WriteLine(option1);
+                    Console.WriteLine(option2);
+                    Console.WriteLine(option3);
+                    Console.WriteLine(option4);
 
 
-               key = Console.ReadKey(true); //Is responsible for reading a key input from the console and storing it in the variable key. If written before while loop it will wait for user input before displaying anything
+                    key = Console.ReadKey(true); //Is responsible for reading a key input from the console and storing it in the variable key. If written before while loop it will wait for user input before displaying anything
 
-                //This switch processes the arrow keys for navigation and the enter key for selection
+                    //This switch processes the arrow keys for navigation and the enter key for selection
                     switch (key.Key)
                     {
                         case ConsoleKey.DownArrow:
@@ -50,94 +55,64 @@
                             break;
                     }
 
+                }
+                Console.WriteLine($"Du valde nummer {option}");
+
+                //This switch handles the options in the menu. Each case corresponds to a menu option and each case implements the logic for each option
+                switch (option)
+                {
+                    case 0:
+                        isProgramRunning = false;
+                        break;
+
+                    case 1:
+                        int age = Helper.TicketType(goBack);
+                        break;
+
+                    case 2:
+                        int totalCost = 0;
+                        Console.WriteLine("Ange antal personer i sällskapet");
+                     string input = Console.ReadLine();
+                        if (int.TryParse(input, out int numberOfPeople))
+                        {
+                            for (int i = 0; i < numberOfPeople; i++)
+                            {
+                                Console.WriteLine($"Ange ålder för person {i + 1}:");
+                                if(int.TryParse(input, out age))
+                                
+                                    totalCost += Helper.CalculatePrice(age);
+                                
+                                age = int.Parse(Console.ReadLine());
+                            }
+                        }
+              
+
+                        Console.WriteLine($" Ni är {numberOfPeople} och totala kostnaden blir {totalCost}");
+                        Console.WriteLine(goBack);
+                        Console.ReadKey(true);
+                        break;
+
+                    case 3:
+                        Helper.RepeatRandomText();
+                        Console.WriteLine(goBack);
+                        Console.ReadKey(true);
+                        break;
+
+                    case 4:
+                        Helper.ExtractThirdWord();
+                        Console.WriteLine(goBack);
+                        Console.ReadKey(true);
+                        break;                       
+                }
+
+              
+             }
+
+
             }
-            Console.WriteLine($"Du valde nummer {option}");
-
-            //This switch handles the options in the menu. Each case corresponds to a menu option and each case implements the logic for each option
-            switch (option)
-            {
-                case 0:
-                    isSelected = true;
-                    break;
-
-                case 1:
-                      Console.WriteLine("Ange din ålder");
-                      int age = int.Parse(Console.ReadLine());
-
-                        if (age < 20)
-                        {
-                            Console.WriteLine("Ungdomspris: 80kr");
-                        }
-
-                        else if (age > 64)
-                        {
-                            Console.WriteLine("Pensionärspris: 90kr");
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("Standardpris: 120kr");
-                        }
 
    
-                    break;
 
-                case 2:
-                    int totalCost = 0;
-                    Console.WriteLine("Ange antal personer i sällskapet");
-                    int numberOfPeople = int.Parse(Console.ReadLine());
-
-                    for (int i = 0; i < numberOfPeople; i++)
-                    {
-                        Console.WriteLine($"Ange ålder för person {i + 1}:");
-                        age = int.Parse(Console.ReadLine());
-
-                        if (age < 20)
-                        {
-                            totalCost += 80;
-                        }
-                        else if (age > 64)
-                        {
-                            totalCost  += 90;
-                        }
-                        else
-                        {
-                            totalCost += 120;
-                        }
-
-                    }
-
-                    Console.WriteLine($" Ni är {numberOfPeople} och totala kostnaden blir {totalCost}");
-                    break;
-
-                    case 3: Console.WriteLine("Ange en random text");
-                    string randomTextInput = Console.ReadLine();
-                    for (int i = 0; i < 10; i++)
-                    {
-                        Console.Write(randomTextInput);
-                    }
-                    break;
-
-                case 4: 
-                    Console.WriteLine("Skriv en mening på minst tre ord");
-                    var userInput = Console.ReadLine(); //Takes in the user input in a var
-                    string[] sentence = userInput.Split(' '); //This splits the word in to an array
-                    var thirdWord = sentence[2]; //And This retrieves the third word at index 2, since index starts at 0
-                   
-                    {
-                        Console.Write($"\nTredje ordet i din mening:{thirdWord}");
-                    }
-                    break;
-
-                    //Behövs default när input saknas? går ändå ur loop med enter.
-                //default:
-                //    Console.WriteLine("Ogiltig Input, försök igen.");
-                //    break;
-            }
-            
-
-        }
-       
     }
 
-}
+    }
